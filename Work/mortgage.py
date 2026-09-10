54acde4ae76897c8a7cad8fdbf579434d1f5ec9e
JuanchyDiaz <juanchydiaztdm@gmail.com>
2026-09-10 02:51:18 -0300

    res ex1.11

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -70,3 +70,36 @@ while principal > 0:
 
 print(f'Total paid {total_paid:.2f}')
 print(f'Months {month}')
+
+
+# Exercise 1.11
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+month = 0
+
+extra_payment = 1000.0
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+
+while principal > 0:
+    month += 1
+    current_payment = payment
+
+    if extra_payment_start_month <= month <= extra_payment_end_month:
+        current_payment += extra_payment
+
+    principal = principal * (1 + rate / 12)
+
+    if principal < current_payment:
+        current_payment = principal
+
+    principal -= current_payment
+    total_paid += current_payment
+
+    print(month, round(total_paid,2), round(principal,2))
+
+print(f'Total paid {total_paid:.2f}')
+print(f'Months {month}')
