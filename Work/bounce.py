b7afb50f45a6b99e5f4ba2c237b22ab8abb5f598
JuanchyDiaz <juanchydiaztdm@gmail.com>
2026-09-10 00:25:55 -0300

    res ej1.5

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,9 @@
 # bounce.py
 #
 # Exercise 1.5
+last_height = 100
+for i in range(10):
+    new_height = last_height * 3/5
+    last_height = new_height
+    print(i + 1, round(last_height, 4))
+    
